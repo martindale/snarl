22eb1d53e70fc5a0c023fc57fdbb20ef9dbb85e0
Chris Vickery <chris@mitrend.com>
2012-10-18 03:41:03 +0100

    Added first versions, it doesnt work yet

---
+++ main.js
@@ -0,0 +1,29 @@
+/*
+Copyright 2012, Coding Soundtrack
+All rights reserved.
+
+Authors:
+
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met: 
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer. 
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution. 
+
+I am not responsible for any cat exploits
+
+*/
+
+// jarPlug
+
+(function($, undefined) {
+if (!jarPlug) return;
+
+console.log('Hey look, I loaded!')
+
+})(jQuery);
+
