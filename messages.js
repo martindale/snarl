5ee6e16f4af4bc2264cbeddf6681cc67cadb16a7
mperry0305 <mperry0305@gmail.com>
2013-01-28 23:51:26 -0500

    Update messages.js
    added 4th option to decrease extraneous donkeyPunch to >10%

--- messages.js
+++ messages.js
@@ -103,10 +103,13 @@ module.exports = {
         }
       var randomSeed = getRandomInt(1, 100);
 
-      if (randomSeed >= 95) {
+      if (randomSeed >= 90) {
         self.chat('you know what, @' + data.from + 'this track IS AWESOME. WOOT');
-      } else if ( randomSeed >= 20 ) {
+      } else if (randomSeed >= 20) {
         self.chat('Agreed, this track is svelte! Wooted.');
+      }else if (randomSeed >=10) {
+        self.chat('Aw yeah, great spin DJ!)
+      }
       } else {
         self.chat('this track is so amazing I might just... DONKEY PUNNNNNCH! ' + randomFact('donkey'));
         self.chat('/me donkeypunches ' + data.from);
