b57d923441b8b8988fa627853192dcb6b0ae7017
Eric Martindale <eric@ericmartindale.com>
2013-03-21 22:11:42 +0000

    fix reconnection timer.

--- bot.js
+++ bot.js
@@ -909,7 +909,7 @@ bot.on('djAdvance', function(data) {
     bot.joinRoom('test', function() {
       bot.joinRoom('coding-soundtrack');
     });
-  }, (data.media + 10) * 1000);
+  }, (data.media.duration + 10) * 1000);
 
   bot.updateDJs(data.djs);
   bot.currentSong = data.media;
