d14804b552a1d8a0cbe03806ca4ff23b5186641f
Eric Martindale <eric@ericmartindale.com>
2016-01-05 11:50:00 -0800

    Migrate eliza plugin to new repo.

--- bin/snarl.js
+++ bin/snarl.js
@@ -4,6 +4,9 @@ var config = require('../config');
 var Snarl = require('../lib/snarl');
 var snarl = new Snarl(config);
 
+// you can add plugins here:
+// snarl.use(require('snarl-karma'));
+
 snarl.start(function(err) {
   console.log('snarl is started.');
 });
