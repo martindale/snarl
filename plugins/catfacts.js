48cc60888912013eeb2abcaaabf6edc3d9041b5a
Nate Richardson <hello@naterichardson.com>
2016-06-03 12:35:54 -0400

    convert api response to valid json object

--- plugins/catfacts.js
+++ plugins/catfacts.js
@@ -3,8 +3,9 @@ var rest = require('restler');
 module.exports = {
   catfacts: function(msg, cb) {
     rest.get('http://catfacts-api.appspot.com/api/facts').on('complete', function(api) {
-      if (api.facts && api.facts.length > 0) {
-        cb(null, api.facts[0]);
+      var input = JSON.parse(api);
+      if (input.facts && input.facts.length > 0) {
+        cb(null, input.facts[0]);
       } else {
         cb(null, 'Damn, the api doesn\'t want to work right now... Not unlike a cat.');
       }
